bc0dc64543165f9a28af365f9a1461e04fcc9794
Shivam Nagar <shivam.nagar@outlook.com>
2023-02-15 21:58:42 +0530

    added default image paths and parametrised the language

--- setup.sh
+++ setup.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+IMAGE=""
+case "$1" in
+  "java")
+    IMAGE="us-west1-docker.pkg.dev/zerok-dev/multi-tier-app/echo-relay-java:latest"
+    ;;
+  "node")
+    IMAGE="shivamnagar/echo-relay:nightly"
+    ;;
+  *)
+    echo "Invalid parameter"
+    exit 1
+    ;;
+esac 
+
 # Enabling istio on default namespace
 kubectl label namespace default "istio-injection=enabled" --overwrite
 
 # performing setup of services
 cd kustomize
 # ./deploy.sh -s service4 -r 1 -f ../service-configurations/service4-definition.yaml
-./deploy.sh -s service1 -i "$1" -r 1 -f ../service-configurations/service1-definition.yaml
-./deploy.sh -s service2 -i "$1" -r 1 -f ../service-configurations/service2-definition.yaml
-./deploy.sh -s service3 -i "$1" -r 1 -f ../service-configurations/service3-definition.yaml
+./deploy.sh -s service1 -i "$IMAGE" -r 1 -f ../service-configurations/service1-definition.yaml
+./deploy.sh -s service2 -i "$IMAGE" -r 1 -f ../service-configurations/service2-definition.yaml
+./deploy.sh -s service3 -i "$IMAGE" -r 1 -f ../service-configurations/service3-definition.yaml
 
 # setting up load test deployment to hit service1
 # kubectl create namespace external
